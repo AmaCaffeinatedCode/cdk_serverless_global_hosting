fbfec98bfd6d13020648c0cbd75c45d087503c42
AmaCaffeinatedCode <amalia.c.villada@gmail.com>
2025-05-08 00:21:06 +0200

    Initial commit: Set up AWS CDK infrastructure for static portfolio website

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import os
 import aws_cdk as cdk
-from portfolio_cdk_stack.portfolio_cdk_stack import PortfolioCdkStack
+from portfolio_cdk_app.portfolio_cdk_app import PortfolioCdkApp
 
 
 app = cdk.App()
-PortfolioCdkStack(app, "PortfolioCdkStackStack")
+PortfolioCdkApp(app, "PortfolioCdkApp")
 app.synth()
--- portfolio_cdk_app/portfolio_cdk_app.py
+++ portfolio_cdk_app/portfolio_cdk_app.py
@@ -9,7 +9,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class PortfolioCdkStack(Stack):
+class PortfolioCdkApp(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id,
             stack_name="PortfolioCdkStack",
